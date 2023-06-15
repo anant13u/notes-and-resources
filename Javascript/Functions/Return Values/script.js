a0be913ac783962d39660a772f726bade5d83c0f
Anant <anant13u@gmail.com>
2023-06-15 17:39:23 +0530

    This commit refactors the existing factorial function and introduces a highlighting feature for displaying the results. The code has been modified to include a new squareroot function using the Math.sqrt method and to format the square root result with three decimal places using the toFixed method.

--- Javascript/Functions/Return Values/script.js
+++ Javascript/Functions/Return Values/script.js
@@ -9,6 +9,11 @@ function cubed(num) {
     return num*num*num
 }
 
+function squareroot(num) {
+    // return num**(1/2)
+    return Math.sqrt(num).toFixed(3)
+}
+
 function factorial(num) {
     if (num<0) return undefined
     if (num==0) return 1
@@ -25,9 +30,10 @@ input.addEventListener("change", () => {
     if (isNaN(num)) {
       para.textContent = "You need to enter a number!";
     } else {
-      para.innerHTML = `${num} squared is ${squared(num)}.<br>`;
-      para.innerHTML += `${num} cubed is ${cubed(num)}.<br>`;
-      para.innerHTML += `${num} factorial is ${factorial(num)}.`;
+      para.innerHTML = `${num} squared is <span class="highlight">${squared(num)}</span>.<br>`;
+      para.innerHTML += `${num} cubed is <span class="highlight">${cubed(num)}</span>.<br>`;
+      para.innerHTML += `Square root of ${num} is <span class="highlight">${squareroot(num)}</span>.<br>`;
+      para.innerHTML += `${num} factorial is <span class="highlight">${factorial(num)}</span>.`;
     }
   });
 
